686d21df3e1dcb2d1ff018985c5331ab0858d1f2
Roman137 <roman.pindak@gmail.com>
2017-10-11 13:37:27 +0300

    [ADDED] Main routing, welcome page

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import {RouterModule} from '@angular/router';
+import {WelcomeComponent} from './welcome/welcome.component';
+
+const appRoutes = [
+  {path: '', redirectTo: '/welcome', pathMatch: 'full'},
+  {path: 'welcome', component: WelcomeComponent},
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(appRoutes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }

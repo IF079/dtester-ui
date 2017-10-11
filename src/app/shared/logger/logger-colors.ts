9043dbd341c1ff2b9183dac950d00d7ea7b1ad88
Roman137 <roman.pindak@gmail.com>
2017-10-11 20:33:12 +0300

    [ADDED] Logger (ng2-logger)

---
+++ src/app/shared/logger/logger-colors.ts
@@ -0,0 +1,9 @@
+export const loggerColors = {
+  service: 'blue',
+  component: 'green',
+  directive: 'orange',
+  http: 'yellow',
+  guard: 'grey',
+  utils: 'brown',
+  resolver: 'aqua'
+};
---
+++ src/app/shared/logger/logger.factory.ts
@@ -0,0 +1,20 @@
+import {Log} from 'ng2-logger';
+import {Logger} from 'ng2-logger/src/logger';
+import {loggerColors} from './logger-colors';
+
+const log = Log.create('LoggerFactory');
+
+export class LoggerFactory {
+  static create(forClass: Function): Logger<any> {
+    const logger = Log.create(forClass.name);
+    const classType = forClass.name.split(/(?=[A-Z])/).pop();
+    logger.color = correspondingColor(classType);
+    log.color = logger.color;
+    log.info(forClass.name, {type: classType, forClass: forClass});
+    return logger;
+  }
+}
+const correspondingColor = (classType: string) => {
+  return loggerColors[classType.toLowerCase()];
+};
+

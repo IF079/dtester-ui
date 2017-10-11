96bc083b3b387ea2237dafaf47eaa40a76a8e904
Andrew Kazmirovich <andreikazmirovich1@gmail.com>
2017-10-12 00:12:14 +0300

    [MODIFIED] Added Students routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import {NgModule} from '@angular/core';
 import {RouterModule} from '@angular/router';
 import {WelcomeComponent} from './welcome/welcome.component';
+import {StudentsComponent} from './students/students.component';
 
 const appRoutes = [
   {path: '', redirectTo: '/welcome', pathMatch: 'full'},
   {path: 'welcome', component: WelcomeComponent},
+  {path: 'students', component: StudentsComponent}
 ];
 
 @NgModule({

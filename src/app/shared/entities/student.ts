44cd5184ccb239ae6f555ff69aea002035c441ca
Andrew Kazmirovich <andreikazmirovich1@gmail.com>
2017-10-11 22:16:03 +0300

    [ADDED] Folder for entities

---
+++ src/app/shared/entities/student.ts
@@ -0,0 +1,10 @@
+export class Student {
+	userId: number;
+	gradebook_id: number;
+	student_surname: string;
+	student_name: string;
+	student_fname: string;
+	group_id: number;
+	plain_password: number;
+	photo: string;
+}

13aaa2d0e646827dff3b49f6c398d1413d378650
Andrew Kazmirovich <andreikazmirovich1@gmail.com>
2017-10-12 20:55:30 +0300

    Try to create setStudent (POST) function

--- src/app/shared/services/students-data.service.ts
+++ src/app/shared/services/students-data.service.ts
@@ -8,14 +8,28 @@ export class StudentsDataService {
 
   URL = '/Student';
 
+  data = JSON.stringify({
+			gradebook_id : "AU-8509358",
+			student_name : "Петро",
+			student_fname : "Петрович",
+			student_surname : "Петренко",
+			plain_password : "123456qwerty",
+			photo: "",
+			group_id : "2"
+  	});
+
   constructor(private http: HttpClient) { }
 
   getStudents(): Observable<Student[]> {
   	return this.http.get(`${this.URL}/getRecords`);
   }
 
-  getStudent(id): Observable<Student> {
+  getStudent(id: number): Observable<Student> {
   	return this.http.get(`${this.URL}/getRecords/${id}`);
+  }
+
+  setStudent(){
+  	return this.http.post(`${this.URL}/insertData`, this.data);
   }
 
 }

268d8d99dd0ef437de98cecb8f93ca23f558ac8c
Andrei <andreikazmirovich1@gmail.com>
2017-10-15 14:30:37 +0300

    [MODIFIED] Added new values

--- src/app/shared/entities/student.ts
+++ src/app/shared/entities/student.ts
@@ -1,10 +1,14 @@
 export class Student {
   userId: number;
+  username: string;
+  password: string;
+  passwordConfirm: string;
+  email: string;
   gradebookId: string;
   studentSurname: string;
   studentName: string;
   studentFname: string;
-  groupId: number;
-  plainPassword: number;
+  groupId: string;
+  plainPassword: string;
   photo: string;
 }

18317d2983a10b0fc64b16a3137a2223cbfd0441
Stefania-26 <stefanijaaa26@gmail.com>
2017-10-18 02:56:09 +0300

    Group Model and Group Grid View Added

---
+++ src/app/groups/groups.component.ts
@@ -0,0 +1,28 @@
+import {Component, OnInit} from '@angular/core';
+import {GroupsService} from './../shared/services/groups.service';
+import {Groups} from './../shared/entities/groups';
+
+
+@Component({
+  selector: 'app-groups',
+  templateUrl: './groups.component.html',
+  styleUrls: ['./groups.component.scss'],
+  providers: [
+    GroupsService
+  ]
+})
+export class GroupsComponent implements OnInit {
+
+  faculties: Groups[];
+
+  displayedColumns = ['Id:', 'Name', 'Faculty id', 'Speciality id', ''];
+
+  constructor(private groupsService: GroupsService ){ }
+  ngOnInit() {
+
+    this.groupsService.getGroups().subscribe(data => {
+      this.groups = data;
+      console.log(this.faculties);
+    });
+  }
+}

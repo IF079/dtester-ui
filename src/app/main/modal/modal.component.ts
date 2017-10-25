583d0e6ca9e347b109cc63e9ad8043e83a03865c
Roman137 <roman.pindak@gmail.com>
2017-10-25 14:43:39 +0300

    [REFACTORED] Modal
    Squashed commit of the following:
    
    commit a70715024c2aa50c5440f770f4d9838fd4c5c0e1
    Author: Roman137 <roman.pindak@gmail.com>
    Date:   Wed Oct 25 14:38:31 2017 +0300
    
        [REFACTORED] Modal

--- src/app/main/modal/modal.component.ts
+++ src/app/main/modal/modal.component.ts
@@ -1,32 +1,21 @@
-import {Component, OnInit} from '@angular/core';
-import {LoggerFactory} from '../../shared/logger/logger.factory';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.scss']
 })
-export class ModalComponent implements OnInit {
+export class ModalComponent {
   isHidden = true;
 
   constructor() {
-    log.info('constructor()');
-  }
-
-  ngOnInit() {
-    log.info('ngOnInit()');
   }
 
   openDialogue() {
-    log.info('openDialogue()');
     this.isHidden = false;
   }
 
   closeDialogue() {
-    log.info('closeDialogue()');
     this.isHidden = true;
   }
-
 }
-
-const log = LoggerFactory.create(ModalComponent);

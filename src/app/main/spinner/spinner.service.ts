cd6401cf7bdb831c617e35dc1a60f5be017bbf1a
Ginungagap <ilya.dog84@gmail.com>
2017-11-04 18:18:35 +0200

    spinner shanged

--- src/app/main/spinner/spinner.service.ts
+++ src/app/main/spinner/spinner.service.ts
@@ -1,39 +1,25 @@
 import {Injectable} from '@angular/core';
 
-import {SPINNER_DEFAULT_CONFIG} from './spinner.default.config';
-
 @Injectable()
 export class SpinnerService {
   private tasksCounter = 0;
-  private takesTooMuchTime = false;
 
   constructor() {
   }
 
   addTask(): void {
     this.tasksCounter++;
-    setTimeout(() => {
-      if (this.hasTasks()) {
-        this.takesTooMuchTime = true;
-      }
-    }, SPINNER_DEFAULT_CONFIG.TIMEOUT);
   }
 
   removeTask(): void {
     if (this.tasksCounter < 0) {
       throw new Error('No tasks to remove');
     }
-    if (this.tasksCounter === 0) {
-      this.takesTooMuchTime = false;
-    }
     this.tasksCounter--;
   }
 
   needsSpinner(): boolean {
-    return this.hasTasks() && this.takesTooMuchTime;
-  }
-
-  private hasTasks(): boolean {
     return this.tasksCounter > 0;
   }
+
 }

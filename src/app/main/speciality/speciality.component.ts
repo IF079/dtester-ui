452e4969d5c31e4373c074f7b42ceed7cbd8c0dc
MariKKKKKKK <mark.prnk@gmail.com>
2017-11-09 15:40:57 +0200

    Speciality pagination added

--- src/app/main/speciality/speciality.component.ts
+++ src/app/main/speciality/speciality.component.ts
@@ -3,14 +3,8 @@ import {Component, OnInit, ViewChild} from '@angular/core';
 import {SpecialityService} from './speciality.service';
 import {Speciality} from './speciality';
 import {generalConst} from '../shared/constants/general-constants';
-import {DataSource} from '@angular/cdk/collections';
-import {Observable} from 'rxjs/Observable';
-import {MatDialog, MatPaginator} from '@angular/material';
-import 'rxjs/add/observable/of';
-import 'rxjs/add/observable/merge';
-import 'rxjs/add/operator/startWith';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/operator/switchMap';
+import {MatDialog} from '@angular/material';
+
 import {RecordsCount} from '../shared/entities/recordsCount';
 import {PageEvent} from '@angular/material';
 
@@ -22,6 +16,11 @@ import {PageEvent} from '@angular/material';
 })
 
 export class SpecialityComponent implements OnInit {
+  limit = 10;
+  pageSizeOptions = [5, 10, 25, 100];
+  offset = 0;
+  errWithDisplayingSubjects: string;
+  numberOfRecords: number;
   specialities: Speciality[];
   pageEvent: PageEvent;
   headingColumnsOfTable = ['№', 'Код', 'Назва', 'edit', 'delete'];
@@ -32,73 +31,31 @@ export class SpecialityComponent implements OnInit {
     placeholderCode: 'Код Спеціальності',
     placeholderName: 'Назва Спеціальності',
   };
-  specialityDatabase: SpecialityDatabase | null;
-  dataSource: SpecialityDataSource | null;
-  errWithDisplayingSpeciality: string;
-  @ViewChild(MatPaginator) paginator: MatPaginator;
 
+  goPage(pageEvent: PageEvent) {
+    this.limit = pageEvent.pageSize;
+    this.offset = ((pageEvent.pageIndex + 1) * pageEvent.pageSize) - pageEvent.pageSize;
+    this.getSpecialities();
+  }
 
   constructor(private specialityService: SpecialityService,
               private dialog: MatDialog) {
 
   }
 
+  getSpecialities(): void {
+    this.specialityService.getSpeciality(this.limit, this.offset).subscribe(data => {
+        this.specialities = data[0];
+        this.numberOfRecords = parseInt(data[1].numberOfRecords, 10);
+      },
+      err => {
+        this.errWithDisplayingSubjects = generalConst.errorWithDisplayData;
+      }
+    );
+  }
+
   ngOnInit(): void {
-    this.specialityDatabase = new SpecialityDatabase(this.specialityService, this.pageEvent.pageIndex);
-    this.dataSource = new SpecialityDataSource(this.specialityDatabase, this.paginator);
+      this.getSpecialities();
   }
 
-
-}
-
-export class SpecialityDatabase {
-  limit = 10;
-  offset = 0;
-
-  constructor(private specialityService: SpecialityService, public pageIndex) {
-
-  }
-
-  getSpecialities(): Observable<[Speciality[], RecordsCount]> {
-    console.log(this.pageIndex);
-      return this.specialityService.getSpeciality(this.limit, this.offset);
-  }
-}
-
-export class SpecialityDataSource extends DataSource<Speciality> {
-  resultsLength = 0;
-  isLoadingResults = false;
-  isRateLimitReached = false;
-
-  constructor(private sDatabase: SpecialityDatabase,
-              private paginator: MatPaginator) {
-    super();
-  }
-
-  connect(): Observable<Speciality[]> {
-    const dSChange = [
-      this.paginator.page,
-    ];
-    return Observable.merge(...dSChange)
-      .startWith(null)
-      .switchMap(() => {
-        this.isLoadingResults = true;
-        return this.sDatabase.getSpecialities();
-      })
-      .map(data => {
-        this.isLoadingResults = false;
-        this.isRateLimitReached = false;
-        this.resultsLength = parseInt(data[1].numberOfRecords, 10);
-
-        return data[0];
-      })
-      .catch(() => {
-        this.isLoadingResults = false;
-        this.isRateLimitReached = true;
-        return Observable.of([]);
-      });
-  }
-
-  disconnect() {
-  }
 }

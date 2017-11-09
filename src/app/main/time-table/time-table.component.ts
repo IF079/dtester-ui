4730ebe3ac9b44bc2c178c99b86e8de9bf89fc7a
rostyslavshkleda <scatrosjob@ukr.net>
2017-11-09 19:57:57 +0200

    pagination for timetable completely done

--- src/app/main/time-table/time-table.component.ts
+++ src/app/main/time-table/time-table.component.ts
@@ -1,16 +1,22 @@
 import {Component, OnInit} from '@angular/core';
+import {MatDialog, MatPaginatorIntl, PageEvent} from '@angular/material';
 
 import {TimeTableService} from './time-table.service';
 import {TimeTable} from './time-table';
 import {generalConst} from '../shared/constants/general-constants';
+import {MatPaginatorIntlUkr} from '../shared/entities/custom-mat-paginator';
 
 @Component({
   selector: 'app-time-table',
   templateUrl: './time-table.component.html',
-  styleUrls: ['./time-table.component.scss']
+  styleUrls: ['./time-table.component.scss'],
+  providers: [{ provide: MatPaginatorIntl, useClass: MatPaginatorIntlUkr}]
 })
 
 export class TimeTableComponent implements OnInit {
+  limit = 10;
+  offset = 0;
+  pageSizeOptions = [5, 10, 25, 100];
   timetables: TimeTable[];
   headingColumnsOfTable = ['№', 'Назва', 'Опис', 'Дата початку', 'Час початку', 'Дата закінчення', 'Час закінчення', '', ''];
   errWithDisplayingTimeTables: string;
@@ -19,8 +25,14 @@ export class TimeTableComponent implements OnInit {
   constructor(private timeTableService: TimeTableService) {
   }
 
+  goPage(pageEvent: PageEvent) {
+    this.limit = pageEvent.pageSize;
+    this.offset = ((pageEvent.pageIndex + 1) * pageEvent.pageSize) - pageEvent.pageSize;
+    this.getTimeTables();
+  }
+
   getTimeTables() {
-    this.timeTableService.getTimeTables(10, 0).subscribe(data => {
+    this.timeTableService.getTimeTables(this.limit, this.offset).subscribe(data => {
         this.timetables = data[0];
         this.numberOfRecords = parseInt(data[1].numberOfRecords, 10);
       },

59557287ae37535074b3da5fac33c1f0159db5cb
rostyslavshkleda <scatrosjob@ukr.net>
2017-11-09 14:27:11 +0200

    pagination for students completely done

--- src/app/main/student/student.component.ts
+++ src/app/main/student/student.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {MatDialog} from '@angular/material';
+import {MatDialog, MatPaginatorIntl, PageEvent} from '@angular/material';
 
 import {StudentService} from './student.service';
 import {GroupsService} from '../groups/groups.service';
@@ -8,14 +8,19 @@ import {LoggerFactory} from '../../shared/logger/logger.factory';
 import {generalConst} from '../shared/constants/general-constants';
 import {StudentAddModalComponent} from './add-modal/add-modal.component';
 import {InfoModalComponent} from './info-modal/info-modal.component';
+import {MatPaginatorIntlUkr} from '../shared/entities/custom-mat-paginator';
 
 @Component({
   selector: 'app-students',
   templateUrl: './student.component.html',
-  styleUrls: ['./student.component.scss']
+  styleUrls: ['./student.component.scss'],
+  providers: [{ provide: MatPaginatorIntl, useClass: MatPaginatorIntlUkr}]
 })
 
 export class StudentComponent implements OnInit {
+  limit = 10;
+  offset = 0;
+  pageSizeOptions = [5, 10, 25, 100];
   students: Student[];
   student: Student;
   headingColumnsOfTable = ['№', '№ Залікової книжки', 'Прізвище', 'Ім\'я', 'По-батькові', '№ групи' , '', ''];
@@ -55,7 +60,6 @@ export class StudentComponent implements OnInit {
     dialogRef.afterClosed().subscribe(result => {
     });
   }
-
   parseGroups(): void {
     this.groupsService.getGroups().subscribe(data => {
       let localArr = [];
@@ -69,8 +73,14 @@ export class StudentComponent implements OnInit {
     });
   }
 
+  goPage(pageEvent: PageEvent) {
+    this.limit = pageEvent.pageSize;
+    this.offset = ((pageEvent.pageIndex + 1) * pageEvent.pageSize) - pageEvent.pageSize;
+    this.getStudents();
+  }
+
   getStudents(): void {
-    this.studentService.getStudents(10, 0).subscribe(data => {
+    this.studentService.getStudents(this.limit, this.offset).subscribe(data => {
         this.students = data[0];
         this.numberOfRecords = parseInt(data[1].numberOfRecords, 10);
         this.students.forEach(item => {

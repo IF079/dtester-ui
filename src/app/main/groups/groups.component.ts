7d958a0c37e53397bb81a63312d37b961e176aae
rostyslavshkleda <scatrosjob@ukr.net>
2017-11-09 20:22:19 +0200

    pagination for groups completely done

--- src/app/main/groups/groups.component.ts
+++ src/app/main/groups/groups.component.ts
@@ -1,24 +1,38 @@
 import {Component, OnInit} from '@angular/core';
+import {MatDialog, MatPaginatorIntl, PageEvent} from '@angular/material';
 
 import {GroupsService} from './groups.service';
 import {Group} from './group';
 import {generalConst} from '../shared/constants/general-constants';
+import {MatPaginatorIntlUkr} from '../shared/entities/custom-mat-paginator';
 
 @Component({
   selector: 'app-groups',
   templateUrl: './groups.component.html',
-  styleUrls: ['./groups.component.scss']
+  styleUrls: ['./groups.component.scss'],
+  providers: [{ provide: MatPaginatorIntl, useClass: MatPaginatorIntlUkr}]
 })
 
 export class GroupsComponent implements OnInit {
+  limit = 10;
+  offset = 0;
+  pageSizeOptions = [5, 10, 25, 100];
   groups: Group[];
   errWithDisplayingGroups: string;
   numberOfRecords: number;
   headingColumnsOfTable = ['№', 'Назва', '№ Факультету', '№ Спеціальності', '', ''];
+
   constructor(private groupsService: GroupsService) {
   }
+
+  goPage(pageEvent: PageEvent) {
+    this.limit = pageEvent.pageSize;
+    this.offset = ((pageEvent.pageIndex + 1) * pageEvent.pageSize) - pageEvent.pageSize;
+    this.getGroupsRange();
+  }
+
   getGroupsRange() {
-    this.groupsService.getGroupsRange(10, 0).subscribe(data => {
+    this.groupsService.getGroupsRange(this.limit, this.offset).subscribe(data => {
         this.groups = data[0];
         this.numberOfRecords = parseInt(data[1].numberOfRecords, 10);
       },

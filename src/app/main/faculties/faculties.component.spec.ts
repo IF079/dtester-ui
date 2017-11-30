112eac63dc063c023f40ac2460aad8db5f679c5f
Ginungagap <ilya.dog84@gmail.com>
2017-12-01 00:27:46 +0200

    faculty service test added

--- src/app/main/faculties/faculties.component.spec.ts
+++ src/app/main/faculties/faculties.component.spec.ts
@@ -1,4 +1,4 @@
-import {TestBed} from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 import {NO_ERRORS_SCHEMA} from '@angular/core';
 
 import {FacultiesComponent} from './faculties.component';
@@ -6,8 +6,12 @@ import {MainMaterialModule} from '../main-material.module';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
 import {FacultyService} from './faculty.service';
 import {UpdateDeleteEntityService} from '../entity-table/update-delete-entity.service';
+import {FacultyServiceMock} from "./faculty.service.mock";
 
 describe('FacultiesComponent', () => {
+
+  let fixture: ComponentFixture<FacultiesComponent>;
+  let component: FacultiesComponent;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -18,15 +22,15 @@ describe('FacultiesComponent', () => {
         HttpClientTestingModule
       ],
       providers: [
-        {provide: FacultyService, /*useValue: new FacultyService()*/},
+        {provide: FacultyService, useValue: new FacultyServiceMock()},
         UpdateDeleteEntityService
       ]
     });
+    fixture = TestBed.createComponent(FacultiesComponent);
+    component = fixture.debugElement.componentInstance;
   });
 
-  xit ('should create component instance', () => {
-    const fixture = TestBed.createComponent(FacultiesComponent);
-    const component = fixture.debugElement.componentInstance;
+  xit ('should create component', () => {
     expect(component).toBeTruthy();
   });
 });
--- src/app/main/faculties/faculty.service.spec.ts
+++ src/app/main/faculties/faculty.service.spec.ts
@@ -1,9 +1,42 @@
-import {TestBed} from '@angular/core/testing';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import {inject, TestBed} from '@angular/core/testing';
 
 import {FacultyService} from './faculty.service';
+import {url} from '../shared/constants/url-constants';
 
 describe( 'FacultyService', () => {
+  let http: HttpTestingController;
+  let facultyService: FacultyService;
+  const getFacultyRequest = httpMock.expectOne(`${url.facultyUrl}${url.getRecordsRange}/1/0`);
+  const getFacultyRecords = httpMock.expectOne(`${url.facultyUrl}${url.getCount}`);
 
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ],
+      providers: [FacultyService]
+    });
 
+    facultyService = TestBed.get(FacultyService);
+    http = TestBed.get(HttpTestingController);
+  });
+
+  it('should be created', inject([FacultyService], (service: FacultyService) => {
+    expect(service).toBeTruthy();
+  }));
+
+  it ('should return faculty and number of records', (done => {
+    facultyService.getFacultiesRange(1, 0).subscribe((res: any) => {
+      done();
+      expect(res[0][0].faculty_name).toEqual('Факультет тестов');
+      expect(res[1].numberOfRecords).toEqual(1);
+    });
+    getFacultyRequest.flush([{
+      faculty_name: 'Факультет тестов',
+      faculty_description: 'Опис'
+    }]);
+    getFacultyRecords.flush({numberOfRecords: 1});
+  });
 
 });
